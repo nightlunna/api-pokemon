f598678c22b3a5ae16f9c3cc9f5ddba306fc383d
Isabelly <isabellyoliveira106@outlook.com>
2026-02-25 13:08:09 -0300

    docs: realizando alguns testes através do insomnia

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class Pokemon(NamedTuple):
     nome: str
     tipo: str
     nivel: int
-    idenficador: str
+    identificador: str
 
 pokemons: list[Pokemon] = []
 
@@ -17,21 +17,52 @@ def criar_pokemon():
     dicionario_pokemon = request.get_json()
 
     novo_pokemon = Pokemon(
-    nome = dicionario_pokemon ("nome"),
-    tipo = dicionario_pokemon ("tipo"),
-    nivel = dicionario_pokemon ("nivel"),
+    nome = dicionario_pokemon.get ("nome"),
+    tipo = dicionario_pokemon.get ("tipo"),
+    nivel = dicionario_pokemon.get ("nivel"),
     identificador = str(uuid.uuid4())
     )
 
     pokemons.append(novo_pokemon)
-    return jsonify({"msg": "Pokémon criado com sucesso!", "pokemon": novo_pokemon._asdict()})
+    return jsonify({"msg": "Pokémon criado com sucesso!", "pokemon": novo_pokemon._asdict()}), 200
 
 
 @app.get("/pokemon/<string:pokemon_id>")
 def get_pokemon_by_id(pokemon_id):
     for pokemon in pokemons:
-        if pokemon.idenficador == pokemon_id:
-            return jsonify(pokemon._asidct())
+        if pokemon.identificador == pokemon_id:
+            return jsonify(pokemon._asdict())
         
     return jsonify({"erro": "Pokémon não encontrado"}), 404
+
+@app.put("/pokemon/<string:pokemon_id>")
+def atualizar_pokemon(pokemon_id):
+    atualizar = request.get_json()
+
+    for indice, pokemon in enumerate(pokemons):
+        if pokemon.identificador == pokemon_id:
+            pokemon_atualizado = Pokemon(
+                nome = atualizar.get("nome", pokemon.nome),
+                tipo = atualizar.get("tipo", pokemon.tipo),
+                nivel = atualizar.get("nivel", pokemon.nivel),
+                identificador = pokemon.identificador 
+            )
+
+            pokemons[indice] = pokemon_atualizado
+
+            return jsonify ({"msg": "Pokémon atualizado!", "pokemon": pokemon_atualizado._asdict()}), 200
+        
+
+@app.delete("/pokemon/<string:pokemon_id>")
+def excluir_pokemon(pokemon_id):
+    for pokemon in pokemons:
+        if pokemon.identificador == pokemon_id:
+            pokemons.remove(pokemon)
+            return jsonify({"msg": "Pokémon excluído com sucesso!"}), 200
+        
+    return jsonify ({"error": "Pokémon não encontrado"}), 404
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
+
     
